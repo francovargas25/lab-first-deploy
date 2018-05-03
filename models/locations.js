f1e3f24284f35f4eec8c546a2328e5fee2602efc
Franco Vargas <francovargas25@gmail.com>
2018-05-03 10:22:06 -0500

    done

--- models/locations.js
+++ models/locations.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 const Schema   = mongoose.Schema;
 
 const locationSchema = new Schema({
-  place: { type: { type: String }, coordinates: [Number] }
+  place: { type: { type: String }, 
+  coordinates: [Number] }
 }, {
   timestamps: { createdAt: "created_at", updatedAt: "updated_at" }
 });
--- routes/siteController.js
+++ routes/siteController.js
@@ -2,7 +2,7 @@ const express = require("express");
 const siteController = express.Router();
 
 siteController.get("/", (req, res, next) => {
-  res.render("site/index", { apiKey: "AIzaSyCTxsFM2Bbm4KEtuFAmdu11WF3s3zUliwU" });
+  res.render("site/index", { apiKey: "AIzaSyD3_SGLq6Z2d7xPDy5YWifonqf4AYT-7nI" });
 });
 
 module.exports = siteController;
